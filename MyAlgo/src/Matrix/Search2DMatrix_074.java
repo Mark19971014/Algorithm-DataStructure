5f74e28a6a8072bcbc82a3fbaeb5eb83a969c8f0
Mark19971014 <s194751@student.dtu.dk>
2024-04-07 18:18:37 +0200

    add test of 074

--- MyAlgo/src/Matrix/Search2DMatrix_074.java
+++ MyAlgo/src/Matrix/Search2DMatrix_074.java
@@ -37,4 +37,13 @@ public class Search2DMatrix_074 {
         return  false;
     }
 
+    public static void main(String[] args) {
+        Search2DMatrix_074 test = new Search2DMatrix_074();
+        int[][] board = {
+                {1,3,5,7},
+                {10,11,16,20},
+                {23,30,34,60},
+        };
+        System.out.println( test.searchMatrix(board,3));
+    }
 }
