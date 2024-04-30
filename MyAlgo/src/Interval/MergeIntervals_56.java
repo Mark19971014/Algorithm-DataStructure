3b6d03770bff7f8db7e087b3493a2220b11ce9cf
Mark19971014 <s194751@student.dtu.dk>
2024-04-30 08:25:06 +0200

    add 56

--- MyAlgo/src/Interval/MergeIntervals_56.java
+++ MyAlgo/src/Interval/MergeIntervals_56.java
@@ -1,83 +1,32 @@
 package Interval;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MergeIntervals_56 {
-
-    public int[][] merge(int[][] interval) {
-
-        sort(interval);
-
-        int length = interval.length;
-
-        int[][] result = new int[length][2];
-        int count = 0; // Track the number of merged intervals
-
-        for (int i = 0; i < length; i++) {
-            if (interval[i] == null) {
-                continue;
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> Interval = new ArrayList<>();
+        int[] curInterval = intervals[0];
+        Interval.add(curInterval);
+        for (int i = 1; i < intervals.length; i++) {
+            //[1,3] [2,6] -> [1,6]
+            if (intervals[i][0] <= curInterval[1]) {
+                // merge the ith and i+1th list
+                curInterval[1] = Math.max(curInterval[1], intervals[i][1]);
             }
-
-            int curStart = interval[i][0];
-            int curEnd = interval[i][1];
-
-            for (int j = i + 1; j < length; j++) {
-                if (interval[j] == null) {
-                    continue;
-                }
-
-                int start2 = interval[j][0];
-                int end2 = interval[j][1];
-
-                if (curStart <= end2 && curEnd >= start2) {
-                    // Overlapping intervals, update the current interval
-                    curStart = Math.min(curStart, start2);
-                    curEnd = Math.max(curEnd, end2);
-
-                    // Mark the interval as merged
-                    interval[j] = null;
-                }
+            else{
+                curInterval = intervals[i];
+                Interval.add(curInterval);
             }
-
-            // Add the merged interval to the result
-            result[count][0] = curStart;
-            result[count][1] = curEnd;
-            count++;
         }
-
-        // Copy the merged intervals to a new array of the correct size
-        int[][] mergedIntervals = new int[count][2];
-        System.arraycopy(result, 0, mergedIntervals, 0, count);
-
-        return mergedIntervals;
+        return Interval.toArray(new int[Interval.size()][]);
     }
-
-    public void sort(int[][] interval){
-        int length = interval.length;
-
-        for(int i  = 1; i < length; i++){
-            int key = interval[i][0];
-            int j = i - 1;
-            int[] keyInterval = interval[i];
-
-            while(j >= 0 && interval[j][0] > key ){
-                interval[j + 1] = interval[j];
-                j--;
-            }
-
-            interval[j + 1] = keyInterval;
-        }
-    }
-
-
     public static void main(String[] args) {
-
-        int[][] interval = {{2,3},{4,5},{6,7},{8,9},{1,10}};
+        int[][] interval = {{1,3},{2,6},{8,10},{15,18}};
         int[][] ans = new int[interval.length][2];
         MergeIntervals_56 test = new MergeIntervals_56();
-        ans = test.merge(interval);
-        for (int i = 0; i < interval.length; i++) {
-            System.out.println(Arrays.toString(ans[i]));
-        }
+        System.out.println(Arrays.deepToString(test.merge(interval)));
     }
 }
